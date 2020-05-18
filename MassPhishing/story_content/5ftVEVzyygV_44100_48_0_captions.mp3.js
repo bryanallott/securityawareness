83c9d6a9bfaeced7bea040784b184498303efa75
Bryan Allott <bryan@theconvergency.co.za>
2020-05-18 14:20:00 +0200

    all files committed

---
+++ MassPhishing/story_content/5ftVEVzyygV_44100_48_0_captions.mp3.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.23.17522.0%0D%0A%0D%0A00:00:00.023%20--%3E%2000:00:01.247%0D%0AI%E2%80%99ll%20look%20at%20it%20and%20get%20back%20to%20you.%0D%0A%0D%0A"}')
